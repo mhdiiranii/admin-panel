13f2725bc7115c6b8ec72f323497fe33dd42091d
MAHDI <mhdiiranii@gmail.com>
2025-03-22 23:03:26 +0330

    design for header and footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
 
 export default function Home() {
   return (
-    <div>
-      home
+    <div className="flex flex-col gap-4">
+      <div className="w-full flex flex-col justify-center items-center p-4">
+        <h1>
+          به پروژه جدید من خوش اومدین
+        </h1>
+      </div>
     </div>
   );
 }
