532058da9ef3040319487f0ab678ff89664d9e35
MAHDI <mhdiiranii@gmail.com>
2025-03-22 17:42:51 +0330

    home page and sign pages

---
+++ src/app/(page)/sign-in/page.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import Button from "@/app/components/button/Button";
+import InputSign from "@/app/components/input/signInput";
+import { SignInSchemaType, signUpSchema } from "@/lib/zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { signIn } from "next-auth/react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import {  useState } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+
+const SignIn = () => {
+  const [errorUser, setErrorUser] = useState<string | undefined>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(signUpSchema),
+  });
+  const router = useRouter();
+  const onSubmit: SubmitHandler<SignInSchemaType> = async (data) => {
+    const newUser = signIn("credentials", {
+      redirect: false,
+      ...data,
+    });
+    const res = await newUser;
+    if (!res?.error) {
+      router.push("/");
+    } else {
+      setErrorUser(res?.error);
+    }
+  };
+  const googleSign = async () => {
+    const user = await signIn("google", { callbackUrl: "/" });
+    console.log(user);
+  };
+  return (
+    <div className="w-full h-screen flex justify-center items-center">
+      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4 px-10 py-8 border rounded-lg ">
+        <InputSign register={register} names="username" placeholder="username" errors={errors} />
+        <InputSign register={register} names="password" type="password" placeholder="password" errors={errors} />
+
+        <div className="w-full flex flex-col gap-4">
+          <Button type="submit" className="bg-green-400 text-white font-bold">
+            Sign In
+          </Button>
+          <Button onClick={googleSign} type="button" className="bg-green-400 text-white font-bold">
+            Google
+          </Button>
+          <Link href={"/sign-up"} className="bg-gray-500 p-2 text-center rounded-lg text-white font-bold hover:shadow-xl duration-200 cursor-pointer">
+            Email
+          </Link>
+          {errorUser !== "" && <span className="text-xs text-right text-red-500">{errorUser}</span>}
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default SignIn;
---
+++ src/app/(page)/sign-up/page.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import { signUpSchema, SignUpSchemaType } from "@/lib/zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useState } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { SignUpUser } from "@/services/User";
+import { useRouter } from "next/navigation";
+import InputSign from "@/app/components/input/signInput";
+import Button from "@/app/components/button/Button";
+import { signIn } from "next-auth/react";
+
+const SignUp = () => {
+  const [errorUser, setErrorUser] = useState<string | undefined>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(signUpSchema),
+  });
+  const router = useRouter();
+
+
+  const onSubmit: SubmitHandler<SignUpSchemaType> = async (data) => {
+
+    
+
+    const newUser = SignUpUser(data);
+    const res = await newUser;
+
+    if (res.ok) {
+      router.push("/sign-in");
+    } else {
+      return setErrorUser(res.message);
+    }
+  };
+
+  const googleSign = async () => {
+    const user = await signIn("google", { callbackUrl: "/" });
+    console.log(user);
+  };
+  return (
+    <div className="w-full h-screen flex justify-center items-center">
+      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4 px-10 py-8 border rounded-lg ">
+        <InputSign register={register} names="email" placeholder="Email" errors={errors} />
+        <InputSign register={register} names="username" placeholder="username" errors={errors} />
+        <InputSign register={register} names="password" type="password" placeholder="Email" errors={errors} />
+        <div className="w-full flex flex-col gap-4">
+          <Button type="submit" className="bg-green-400 text-white font-bold">
+            Sign Up
+          </Button>
+          <Button type="button" onClick={googleSign} className="bg-green-400 text-white font-bold">Google</Button>
+          {errorUser !== "" && <span className="text-xs text-right text-red-500">{errorUser}</span>}
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default SignUp;
